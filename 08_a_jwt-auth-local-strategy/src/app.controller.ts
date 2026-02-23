421994c068f16f39912763c4f38f0c1517dfbd38
Rushikesh Surve <rushikesh.surve@shwaira.com>
2026-02-23 13:16:42 +0530

    Return JWT access_token on success login, use @nestjs/jwt

--- 08_a_jwt-auth-local-strategy/src/app.controller.ts
+++ 08_a_jwt-auth-local-strategy/src/app.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 // import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
 
+  constructor(private authService: AuthService) { }
   // AuthGuard - NestJS factory function (mixin) - returns class
   // @UseGuards(AuthGuard("local"))  // No hardcoding magic string
   @UseGuards(LocalAuthGuard)
   @Post("auth/login")
   async login(@Request() req) {
-    return req.user
+    return this.authService.login(req.user)
   }
 
   @UseGuards(LocalAuthGuard)
---
+++ 08_a_jwt-auth-local-strategy/src/auth/constants.ts
@@ -0,0 +1,4 @@
+// 08_a_jwt-auth-local-strategy/src/auth/constants.ts
+export const jwtConstants = {
+    secret: "very-secure-secret"
+}
