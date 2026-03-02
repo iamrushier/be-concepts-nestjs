35a56655b4c3139177cf8f8ddfef8c037c566815
Rushikesh Surve <rushikesh.surve@shwaira.com>
2026-03-02 16:17:38 +0530

    Jwt module setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { RefreshToken } from './refresh-token.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User, RefreshToken])],
+    imports: [ // Dynamic modules - returned by methods
+        TypeOrmModule.forFeature([User, RefreshToken]),
+        JwtModule.register({
+            secret: "random-jwt-secret"
+        })
+    ],
     exports: [RefreshToken]
 })
 export class AuthModule { }
